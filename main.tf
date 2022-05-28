e643e0f18f0414a15b95dc2b00a2ed1802256f08
maurits-cottyn <maurits.cottyn@devoteam.com>
2022-05-28 10:23:52 +0000

    chore: fixed indentation

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ module "vpc" {
   source    = "../tf-gcp-modules-network-vpc"
   vpc       = var.vpc
   firewalls = var.firewalls
-  context = module.this.context
+  context   = module.this.context
 }
 
 module "vm" {
-  source = "../tf-gcp-modules-compute-engine"
+  source    = "../tf-gcp-modules-compute-engine"
   instances = var.vms
-  context = module.this.context
+  context   = module.this.context
 }
